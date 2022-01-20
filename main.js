180e3c7dd665f16d23152120c0c346d229755e25
antoniom28 <antomarc@gmail.com>
2022-01-20 15:04:17 +0100

    aggiunta pagina first-access, dayjs da sistemare

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let allContacts = [
         visible: true,
         chatOpen: false,
         lastMessage: null,
-        statusText : "il primo test è sempre il più bello",
+        statusText: "il primo test è sempre il più bello",
         messages: [
             {
                 date: '10/01/2020 15:30:55',
@@ -30,7 +30,7 @@ let allContacts = [
         visible: true,
         chatOpen: false,
         lastMessage: null,
-        statusText : "se potessi fermare il tempo come una fotografia, prenderei questo momento, ci farei un'enorme gigantografia.",
+        statusText: "se potessi fermare il tempo come una fotografia, prenderei questo momento, ci farei un'enorme gigantografia.",
         messages: [
             {
                 date: '20/03/2020 16:30:00',
@@ -55,7 +55,7 @@ let allContacts = [
         visible: true,
         chatOpen: false,
         lastMessage: null,
-        statusText : "non c'è futuro per chi vive nel passato",
+        statusText: "non c'è futuro per chi vive nel passato",
         messages: [
             {
                 date: '28/03/2020 10:10:40',
@@ -80,16 +80,11 @@ let allContacts = [
         visible: true,
         chatOpen: false,
         lastMessage: null,
-        statusText : "non chatto con nessuno, figurati se metto uno stato",
+        statusText: "non chatto con nessuno, figurati se metto uno stato",
         messages: [
         ], //test su nuova chat
     },
 ]
-
-const months = [
-    "January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December"
-];
 
 let weekdays = [
     "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
@@ -100,30 +95,29 @@ var app = new Vue(
         el: "#root",
         data: {
             contacts: allContacts,
-            chat: allContacts[0],
+            chat: null,
             inputText: null,
             searchBar: null,
             dateToday: dayjs(),
             infoMess: false,
             infoProfile: false,
-            inputImage : null,
+            inputImage: null,
             infoMessage: {
                 text: null,
                 date: null,
                 status: null,
-                image : null,
+                image: null,
             },
             infoProfileObj: {
-                name : null,
-                image : null,
-                statusText : null,
-                imageSent : [],
+                name: null,
+                image: null,
+                statusText: null,
+                imageSent: [],
             },
         },
         methods: { //AL CLICK APRE LA CHAT CORRISPONDENTE
             openChat: function (elemento) {
-                this.infoMess = false;
-                this.infoProfile = false;
+                this.switchInfo('all');
                 this.chat = elemento;
                 for (let i = 0; i < this.contacts.length; i++)
                     this.contacts[i].chatOpen = false;
@@ -132,7 +126,7 @@ var app = new Vue(
             writeMessage: function (elemento) {
                 //PERMETTE DI SCRIVERE MESSAGGI IN CHAT
                 let controlloTesto = null;
-                if(this.inputText != "" && this.inputText != null)
+                if (this.inputText != "" && this.inputText != null)
                     controlloTesto = this.inputText.replace(/\s/g, '');
                 else
                     return;
@@ -149,14 +143,14 @@ var app = new Vue(
                 }
             },
             //volevo fare in modo che capisse che file gli
-            //arrivasse ma, sono cose che credo faremo
-            //quindi ho lasciato solo l'imm 
-            sendImage: function (elemento,target) {
+            //arrivasse ma, sono cose che credo faremo più avanti
+            //quindi ho lasciato solo l'immagine
+            sendImage: function (elemento, target) {
                 let input = null;
                 let error = "image";
-                if(target == 'background')
+                if (target == 'background')
                     input = document.getElementById("input-image-profile");
-                else if(target == 'send')
+                else if (target == 'send')
                     input = document.getElementById("input-image");
                 else
                     error = "ci scusiamo per il problema";
@@ -170,11 +164,11 @@ var app = new Vue(
                         status: 'sent',
                         image: event.target.result,
                     };
-                    if(target == 'send'){
+                    if (target == 'send') {
                         elemento.messages.push(newMessage);
                         app.dateFormat();
                         app.reply(elemento);
-                    } else{
+                    } else {
                         document.getElementById('user-profile-picture').style.backgroundImage = `url(${newMessage.image})`;
                     }
                     input.value = null;
@@ -210,8 +204,7 @@ var app = new Vue(
                             this.contacts[i].visible = false;
             },
             showMessageMenu: function (index, hover) {
-                //PERMETTE L'APERTURA DEL MENU SUL CLICK
-                //SU UN MESSAGGIO
+                //PERMETTE L'APERTURA DEL MENU SUL CLICK SU UN MESSAGGIO
                 let menu = document.getElementsByClassName('message-button');
                 if (hover)
                     menu[index].style.display = "flex";
@@ -220,7 +213,7 @@ var app = new Vue(
             },
             messageMenu: function (index) {
                 //MENU SUL CLICK
-                if(!this.infoMess || !this.infoProfile){
+                if (!this.infoMess || !this.infoProfile) {
                     let menu = document.getElementsByClassName('message-menu');
                     //sposta il pannello del cancella messaggio correttamente, 
                     //sopra se si supera la metà della heigth della finestra,
@@ -238,24 +231,16 @@ var app = new Vue(
                             menu[index].className += ' visible';
                         }, 0); //aggiungle la classe dopo l'add event a 179js
                     }
-                } else{
-                    this.infoMess = false;
-                    this.infoProfile = false;
-                }
+                } else 
+                    this.switchInfo('all');
             },
             showInfo: function (mess) {
                 this.switchInfo('message');
                 const maxW = document.documentElement.clientWidth;
                 let mainChatWidth = document.getElementById('your-chat').clientWidth;
                 setTimeout(() => {
-                    if (maxW <= 992) {
-                        document.getElementById('show-info-box').style.left = `${maxW / 2 - 100}px`;
-                        document.getElementById('show-info-box').style.flex = `1 0 ${mainChatWidth}px`;
-                        setTimeout(() => {
-                            mainChatWidth = document.getElementById('your-chat').clientWidth;
-                            document.getElementById('show-info-box').style.left = `-${mainChatWidth + 1}px`;
-                        }, 10);
-                    }
+                    let elemento = document.getElementById('show-info-box');
+                    this.slideInfoBox(elemento, mainChatWidth, maxW);
                     this.infoMessage.text = mess.text;
                     this.infoMessage.date = mess.date;
                     this.infoMessage.status = mess.status;
@@ -263,48 +248,53 @@ var app = new Vue(
                 }, 10);
             },
             showInfoProfile: function (chat) {
-                if(!this.infoProfile){
-                const maxW = document.documentElement.clientWidth;
-                let mainChatWidth = document.getElementById('your-chat').clientWidth;
-                setTimeout(() => {
-                    if (maxW <= 992) {
-                        document.getElementById('show-profile-info-box').style.left = `${maxW / 2 - 100}px`;
-                        document.getElementById('show-profile-info-box').style.flex = `1 0 ${mainChatWidth}px`;
-                        setTimeout(() => {
-                            mainChatWidth = document.getElementById('your-chat').clientWidth;
-                            document.getElementById('show-profile-info-box').style.left = `-${mainChatWidth + 1}px`;
-                        }, 10);
-                    }
-
-                    this.infoProfileObj.name = chat.name;
-                    this.infoProfileObj.image = chat.avatar;
-                    this.infoProfileObj.statusText = chat.statusText;
-                    this.infoProfileObj.imageSent = [];
-                    for(let i=0; i<chat.messages.length; i++){
-                        let erroreStessaImmagine = false;
-                        if(chat.messages[i].image != undefined){
-                            for(let j=0; j<this.infoProfileObj.imageSent.length; j++){
-                                if(this.infoProfileObj.imageSent[j] == chat.messages[i].image)
-                                    erroreStessaImmagine = true;
-                            }
-                            if(!erroreStessaImmagine){
-                                this.infoProfileObj.imageSent.push(chat.messages[i].image);
-                                console.log(this.infoProfileObj.imageSent);
+                if (!this.infoProfile) {
+                    const maxW = document.documentElement.clientWidth;
+                    let mainChatWidth = document.getElementById('your-chat').clientWidth;
+                    setTimeout(() => {
+                        let elemento = document.getElementById('show-profile-info-box');
+                        this.slideInfoBox(elemento, mainChatWidth, maxW);
+                        this.infoProfileObj.name = chat.name;
+                        this.infoProfileObj.image = chat.avatar;
+                        this.infoProfileObj.statusText = chat.statusText;
+                        this.infoProfileObj.imageSent = [];
+                        for (let i = 0; i < chat.messages.length; i++) {
+                            let erroreStessaImmagine = false;
+                            if (chat.messages[i].image != undefined) {
+                                for (let j = 0; j < this.infoProfileObj.imageSent.length; j++) {
+                                    if (this.infoProfileObj.imageSent[j] == chat.messages[i].image)
+                                        erroreStessaImmagine = true;
+                                }
+                                if (!erroreStessaImmagine) {
+                                    this.infoProfileObj.imageSent.push(chat.messages[i].image);
+                                    console.log(this.infoProfileObj.imageSent);
+                                }
                             }
                         }
-                    }
-                }, 10);
+                    }, 10);
                 }
-                
                 this.switchInfo('profile');
             },
+            slideInfoBox: function (elemento, mainChatWidth, maxW) {
+                if (maxW <= 992) {
+                    elemento.style.left = `${maxW / 2 - 100}px`;
+                    elemento.style.flex = `1 0 ${mainChatWidth}px`;
+                    setTimeout(() => {
+                        mainChatWidth = document.getElementById('your-chat').clientWidth;
+                        elemento.style.left = `-${mainChatWidth + 1}px`;
+                    }, 10);
+                }
+            },
             switchInfo: function (infoType) {
-                if(infoType == 'message'){
+                if (infoType == 'message') {
                     this.infoMess = !this.infoMess;
                     this.infoProfile = false;
                 }
-                else{
+                else if(infoType == 'profile'){
                     this.infoProfile = !this.infoProfile;
+                    this.infoMess = false;
+                } else if(infoType == 'all'){
+                    this.infoProfile = false;
                     this.infoMess = false;
                 }
             },
@@ -353,24 +343,19 @@ var app = new Vue(
 
             },
             resize: function () {
-                this.infoMess = false;
-                this.infoProfile = false;
+                this.switchInfo('all');
             }
         },
         updated: function () {
-            //rimuove il menu al cambio chat, 
-            //perché se si lasciava il pannello
-            //del menu sul messaggio, rimaneva anche in un
-            //messaggio a caso su un'altra chat
+            //rimuove il menu-box al cambio chat
             let menu = document.getElementsByClassName('message-menu');
-            for (let i = 0; i < menu.length; i++){
+            for (let i = 0; i < menu.length; i++) {
                 menu[i].style.display = "none";
                 menu[i].classList.remove('visible');
             }
         },
         beforeCreate: function () {
-            //evento click globale, toglie tutti i pannelli
-            //del menu
+            //evento click globale, toglie tutti i pannelli del menu
             window.addEventListener('click', function () {
                 let menu = document.getElementsByClassName('message-menu');
                 for (let i = 0; i < menu.length; i++)
